e6bbf32ced66a4d667a587b93ae87a3003453f83
Cristian <cperez90@alumnes.politecnicllevant.cat>
2025-10-11 19:37:47 +0200

    getter y setter y el serialized

--- src/main/java/org/daw/model/MediaItem.java
+++ src/main/java/org/daw/model/MediaItem.java
@@ -1,6 +1,13 @@
 package org.daw.model;
 
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class MediaItem {
+    @SerializedName("mal_id")
     protected int malId;
     protected String url;
 }
